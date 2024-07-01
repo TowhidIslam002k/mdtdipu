eb3f08df1864769817b6d81f7d8884881931ba51
Md Towhid Islam <115937153+TowhidIslam002k@users.noreply.github.com>
2024-07-01 23:34:21 +0600

    uploading all files

---
+++ js/typed.js
@@ -0,0 +1,9 @@
+(function($){
+    $(document).ready(function(){
+    var typed = new Typed('.typed', {
+    strings: ['Towhid Islam', 'WEB DEVELOPER', 'UI/UX DESIGNER'],
+    typeSpeed: 100,
+    loop: true,
+    });
+});  
+})(jQuery)
